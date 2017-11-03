a2c3d9b4f17ebe12e4d17f8537d5742d31f7d412
bill-wangbiao <it2907@163.com>
2017-11-03 15:38:27 +0800

    update codes

--- src/test/java/com/bill/test/mq/MqProducerTest.java
+++ src/test/java/com/bill/test/mq/MqProducerTest.java
@@ -4,24 +4,28 @@ package com.bill.test.mq;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 public class MqProducerTest {
 	private final static String QUEUE_NAME = "hello";
 
 	public static void main(String[] argv) throws Exception {
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+		factory.setHost("192.168.1.129");
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
-
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		/**消息持久化：durable和下面的MessageProperties.PERSISTENT_TEXT_PLAIN配置
+		 * 作用：保证消息在mqserver宕机的情况下，消息不丢失；
+		 * 实现原理：把消息保存在磁盘上，记住消息的状态和内容**/
+		boolean durable=true;
+		channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
 		String message = "Hello World!";
 		for(int i=0;i<10;i++) {
-			channel.basicPublish("", QUEUE_NAME, null, (message+i).getBytes("UTF-8"));
-		}
-		
-		System.out.println("发送完成"+message);
+			//MessageProperties.PERSISTENT_TEXT_PLAIN
+			channel.basicPublish("", QUEUE_NAME,null , (message+i).getBytes("UTF-8"));
+			System.out.println("发送完成"+message+i);
 
+		}
 		channel.close();
 		connection.close();
 	}
